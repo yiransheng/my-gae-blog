a789b0066bef23911dc2664c473a48089b0ce5c2
Rishabh Manocha <rmanocha@gmail.com>
2012-03-29 23:34:13 -0400

    Adding 404 and 500 error handlers

--- simple.py
+++ simple.py
@@ -17,6 +17,17 @@ POSTS_PER_PAGE = app.config.get('POSTS_PER_PAGE', 10)
 
 _punct_re = re.compile(r'[\t !"#$%&\'()*\-/<=>?@\[\\\]^_`{|},.]+')
 
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return flask.render_template("404.html"), 404
+
+
+@app.errorhandler(500)
+def server_error(e):
+    return flask.render_template("500.html"), 500
+
+
 class Post(ndb.Model):
     title = ndb.StringProperty(required=True, indexed=False)
     slug = ndb.StringProperty(required=True)
