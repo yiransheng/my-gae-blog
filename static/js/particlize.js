d6a8d520e118b1fbbb2878a3b04ae3258d13f80a
Yiran Sheng <me@yiransheng.com>
2012-04-07 18:48:37 -0400

    update animatated logo

--- static/js/particlize.js
+++ static/js/particlize.js
@@ -6,8 +6,8 @@ IMG.src = $("#logo")[0].src;
 $(IMG).bind("load", function() {
       var ctx,canvas,Particle,Sys,W,H,p,sys,tl, 
           redValueForPixle,imageData, 
-          pixels, spawn, image = this, x, y, colorR,
-          colorG, colorB, colorA, srcImg = $("#logo")[0];
+          pixels, spawn, image = this,x,y,xR,yR,l,
+          line,start,end,srcImg = $("#logo")[0]; 
 
       canvas = $('#stage');
       if (! (canvas[0].getContext && (ctx=canvas[0].getContext("2d"))) ) {
@@ -17,29 +17,34 @@ $(IMG).bind("load", function() {
       
       W = srcImg.offsetWidth || 200;
       H = srcImg.offsetHeight || 200;
+      xR=Sheng.point(W/2,0);
+      yR=Sheng.point(0,H/2);
 
       $("#stage-wrapper").css({
           position: "absolute", 
 	  top: "29px", 
 	  left: "93px", 
-	  backgroundImage: image.src, 
-	  backgroundRepeat: "no-repeat" 
+      });
+      canvas.css({
+	  display: "none",
       });
       
       canvas[0].width = W;
       canvas[0].height = H;
 
       Particle = Sheng.Particle.extend({
-          colorR : 0, 
-	  colorG : 0, 
-	  colorB : 0, 
-	  colorA : 0
+	  line: null, 
+	  dest: null
       });
       Sys = Sheng.ParticleSys.extend({
+	  diff: 0, 
+	  j1: 15, 
+	  j2: 85, 
+	  on: false,
           outBound: function(p) {
-              var np = p.p.clone().add(p.v).subtract(Sheng.point(W/2,H/2));
-	      if (np.length()>W/2) {
-	          p.bounce(np);
+	      var npx = p.p.x + p.v.x;
+	      if (npx<-W/2 || npx>W/2) {
+		  p.v.x*=-1;
 		  return true
 	      } 
 	      return false
@@ -47,24 +52,41 @@ $(IMG).bind("load", function() {
 
 	  updateAll: function() {
               
-	      var a, i, j, p, d;
+	      var a,i,j,k,p,mk,d,
+	          p1 = this.diff<60 ? Math.random()*W : 0, 
+	          p2 = this.diff<60 ? Math.random()*W : 0;
+	      this.j1 = this.j1>=this.j2-5 ? Math.min(p1,p2) : this.j1;
+	      this.j2 = this.j2<=this.j1+5 ? Math.max(p1,p2,this.j1+15) : this.j2;
+
 	      for (j=0;j<this.particles.length;j++) {
 	          p = this.particles[j];
-		  p.update();
-		  d = p.dest.clone().subtract(p.p).scale(0.06);
-		  p.v.add(d);
-		  p.v.scale(Math.random()*.1+.9);
-		  if ((a=d.length())<0.01){
-		      p.v.set(Math.random()*50-15, Math.random()*10-5);
-		      p.p.set(p.dest.clone().scale(.5));
+		  p.p.x += p.v.x;
+		  d = ( - p.p.x)*(Math.random()*0.05+0.05);
+		  p.v.x+=d;
+		  p.v.x*=(Math.random()*.1+.9);
+		  d = Math.abs(d);
+		  this.diff *= 0.9;
+		  if (this.diff<=2 && d<0.05) {
+		      p.v.x = Math.random()*3-1.5;
+		  } else if (d<0.05 && j>this.j1 && j<this.j2) {
+                      p.p.x = Math.random()*W*0.75-W*.35;
+		      p.v.x = Math.random()*10 - 5;
+		      this.j1+=Math.random();
+		      this.j2-=Math.random();
+		  } else {
+		      this.diff+=d*5;
 		  }
-		  i = redValueForPixel(p.p.x, p.p.y);
-		  this.imageData.data[i] = p.colorR;
-		  this.imageData.data[i+1] = p.colorG;
-		  this.imageData.data[i+2] = p.colorB;
-		  this.imageData.data[i+3] = Math.round((1-a/3)*p.colorA);
+		  this.diff=this.diff>99 ? 99 : this.diff;
+		  i = 4*j*W;
+		  for (k=p.beg*4;k<p.end*4;k++){
+		      mk = k - Math.round(p.p.x)*4;
+		      if (mk>=0 && mk <W*4) {
+		          this.imageData.data[i+k]=p.line[mk]*(1-d/3);
+		      } else {
+		          this.imageData.data[i+k]=180;
+		      }
+		  }
 	      }
-
 	      ctx.putImageData(this.imageData, 0, 0);  
 	  }
       });
@@ -77,8 +99,8 @@ $(IMG).bind("load", function() {
           sys.updateAll();
       });
       redValueForPixel = function(x,y) { 
-	  x = Math.floor(x);
-	  y = Math.floor(y);
+	  x = Math.round(x);
+	  y = Math.round(y);
           return ((y - 1) * (W * 4)) + ((x - 1) * 4) 
       }; 
 
@@ -87,37 +109,48 @@ $(IMG).bind("load", function() {
       imageData = ctx.getImageData(0, 0, W, H);  
       pixels = imageData.data;  
       sys.imageData = imageData;
-      for (x=1;x<=imageData.width;x++){
-	  for (y=1;y<imageData.height;y++){
-	      colorR = pixels[redValueForPixel(x,y)];
-	      colorG = pixels[redValueForPixel(x,y)+1];
-	      colorB = pixels[redValueForPixel(x,y)+2];
-	      colorA = pixels[redValueForPixel(x,y)+3];
-	      if (colorA >.05) {
+      for (y=1;y<=imageData.height;y++){
+	  // for (x=1;x<imageData.width;x++){
+	      start = (y-1)*W*4;
+	      end = start+W*4;
+              line = Array.prototype.slice.apply(pixels, [start, end]);
+	      l = 2*y/H - 1;
+	      l = Math.acos(l); 
+	      l = Math.sin(l)*H/2;
+	      // if (colorA >.05) {
 		  p = Sheng.create(Particle, {
-		      p : Sheng.point(x,y),
-		      v : Sheng.point(Math.random()*W/10-W/14, Math.random()*H/20-H/30),
-		      a : Sheng.point(-0.1,0),
-		      colorR : colorR,
-		      colorG : colorG,
-		      colorB : colorB,
-		      colorA : colorA
+		      p : Sheng.point(0,y),
+		      v : Sheng.point(0,0),
+		      a : null,
+		      beg : Math.round(W/2-l),
+		      end : Math.round(W/2+l), 
+		      line: line
 		  });    
-		  p.dest = Sheng.point(x,y);
+		  p.dest = Sheng.point(0,y);
 		  sys.add(p);
-	      }
-	  }
+	      // }
+	//  }
       }
-
       $("#stage-wrapper").bind("mouseover", function(e){
           e.preventDefault();
-	  tl.start();
-	  $("#stage").fadeIn("slow");
+	  var el = canvas || $("#stage");
+	  if ( !el.is(":animated") ) {
+	      tl.start();
+	      el.fadeIn("slow");
+	  } else {
+	      tl.start();
+	  }
       });
       $("#stage-wrapper").bind("mouseout", function(e){
           e.preventDefault();
-	  tl.stop();
-	  $("#stage").fadeOut("slow");
+	  var el = canvas || $("#stage");
+	  if ( !el.is(":animated") ){
+	      el.fadeOut("slow", function() {
+	          tl.stop();
+	      });
+	  } else {
+	      tl.stop();
+	  }
       });
 });
 
